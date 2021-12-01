63e89892c79ac3011f907f7d8a2d78a0c02e65bd
frankennstein <karastoqnov.alexandar@gmail.com>
2021-12-01 23:54:25 +0200

    Last Update: 01.12.2021

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 import tkinter.font as tkFont
+import sys
+
 
 class App:
     def __init__(self, root):
         #setting title
-        root.title("undefined")
+        root.title("Game of Rock, Paper, Scissors")
         #setting window size
         width=600
         height=500
@@ -14,25 +16,25 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GButton_632=tk.Button(root)
-        GButton_632["activebackground"] = "#ff5722"
-        GButton_632["activeforeground"] = "#000000"
-        GButton_632["bg"] = "#efefef"
+        exitButton=tk.Button(root)
+        exitButton["activebackground"] = "#ff5722"
+        exitButton["activeforeground"] = "#000000"
+        exitButton["bg"] = "red"
         ft = tkFont.Font(family='Times',size=10)
-        GButton_632["font"] = ft
-        GButton_632["fg"] = "#000000"
-        GButton_632["justify"] = "center"
-        GButton_632["text"] = "Button"
-        GButton_632.place(x=500,y=450,width=70,height=25)
-        GButton_632["command"] = self.GButton_632_command
+        exitButton["font"] = ft
+        exitButton["fg"] = "#000000"
+        exitButton["justify"] = "center"
+        exitButton["text"] = "Exit"
+        exitButton.place(x=500,y=450,width=70,height=25)
+        exitButton["command"] = self.exitButton_command
 
-        GLabel_650=tk.Label(root)
+        topLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_650["font"] = ft
-        GLabel_650["fg"] = "#333333"
-        GLabel_650["justify"] = "center"
-        GLabel_650["text"] = "label"
-        GLabel_650.place(x=40,y=30,width=524,height=119)
+        topLabel["font"] = ft
+        topLabel["fg"] = "#333333"
+        topLabel["justify"] = "center"
+        topLabel["text"] = "label"
+        topLabel.place(x=40,y=30,width=524,height=119)
 
         GButton_30=tk.Button(root)
         GButton_30["bg"] = "#efefef"
@@ -64,32 +66,32 @@ class App:
         GButton_519.place(x=420,y=160,width=140,height=126)
         GButton_519["command"] = self.GButton_519_command
 
-        GLabel_819=tk.Label(root)
+        lastGameLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_819["font"] = ft
-        GLabel_819["fg"] = "#333333"
-        GLabel_819["justify"] = "center"
-        GLabel_819["text"] = "label"
-        GLabel_819.place(x=40,y=300,width=519,height=52)
+        lastGameLabel["font"] = ft
+        lastGameLabel["fg"] = "#333333"
+        lastGameLabel["justify"] = "center"
+        lastGameLabel["text"] = "label"
+        lastGameLabel.place(x=40,y=300,width=519,height=52)
 
-        GLabel_676=tk.Label(root)
+        lastGameMessage=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_676["font"] = ft
-        GLabel_676["fg"] = "#333333"
-        GLabel_676["justify"] = "center"
-        GLabel_676["text"] = "label"
-        GLabel_676.place(x=140,y=360,width=320,height=30)
+        lastGameMessage["font"] = ft
+        lastGameMessage["fg"] = "#333333"
+        lastGameMessage["justify"] = "center"
+        lastGameMessage["text"] = "label"
+        lastGameMessage.place(x=140,y=360,width=320,height=30)
 
-        GLabel_270=tk.Label(root)
+        currentResult=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_270["font"] = ft
-        GLabel_270["fg"] = "#333333"
-        GLabel_270["justify"] = "center"
-        GLabel_270["text"] = "label"
-        GLabel_270.place(x=20,y=390,width=233,height=83)
+        currentResult["font"] = ft
+        currentResult["fg"] = "#333333"
+        currentResult["justify"] = "center"
+        currentResult["text"] = "label"
+        currentResult.place(x=20,y=390,width=233,height=83)
 
-    def GButton_632_command(self):
-        print("command")
+    def exitButton_command(self):
+        return sys.exit()
 
 
     def GButton_30_command(self):
