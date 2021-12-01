202b2fdcb206ec2801aa66e01f384510d256794b
frankennstein <karastoqnov.alexandar@gmail.com>
2021-12-02 00:09:08 +0200

    Last update: 02/12/2021

--- gui.py
+++ gui.py
@@ -33,45 +33,45 @@ class App:
         topLabel["font"] = ft
         topLabel["fg"] = "#333333"
         topLabel["justify"] = "center"
-        topLabel["text"] = "label"
+        topLabel["text"] = "Compete against the machines and do not allow them to take over the world!\nThe humanity is counting on you soldier!"
         topLabel.place(x=40,y=30,width=524,height=119)
 
-        GButton_30=tk.Button(root)
-        GButton_30["bg"] = "#efefef"
+        rockButton=tk.Button(root)
+        rockButton["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
-        GButton_30["font"] = ft
-        GButton_30["fg"] = "#000000"
-        GButton_30["justify"] = "center"
-        GButton_30["text"] = "Button"
-        GButton_30.place(x=40,y=160,width=142,height=128)
-        GButton_30["command"] = self.GButton_30_command
+        rockButton["font"] = ft
+        rockButton["fg"] = "#000000"
+        rockButton["justify"] = "center"
+        rockButton["text"] = "Try hit them with a rock!"
+        rockButton.place(x=40,y=160,width=142,height=128)
+        rockButton["command"] = self.rockButton_command
 
-        GButton_235=tk.Button(root)
-        GButton_235["bg"] = "#efefef"
+        paperButton=tk.Button(root)
+        paperButton["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
-        GButton_235["font"] = ft
-        GButton_235["fg"] = "#000000"
-        GButton_235["justify"] = "center"
-        GButton_235["text"] = "Button"
-        GButton_235.place(x=230,y=160,width=143,height=127)
-        GButton_235["command"] = self.GButton_235_command
+        paperButton["font"] = ft
+        paperButton["fg"] = "#000000"
+        paperButton["justify"] = "center"
+        paperButton["text"] = "Who said the pen is\nmightier than the sword?!"
+        paperButton.place(x=230,y=160,width=143,height=127)
+        paperButton["command"] = self.paperButton_command
 
-        GButton_519=tk.Button(root)
-        GButton_519["bg"] = "#efefef"
+        scissorsButton=tk.Button(root)
+        scissorsButton["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
-        GButton_519["font"] = ft
-        GButton_519["fg"] = "#000000"
-        GButton_519["justify"] = "center"
-        GButton_519["text"] = "Button"
-        GButton_519.place(x=420,y=160,width=140,height=126)
-        GButton_519["command"] = self.GButton_519_command
+        scissorsButton["font"] = ft
+        scissorsButton["fg"] = "#000000"
+        scissorsButton["justify"] = "center"
+        scissorsButton["text"] = "Ok, time to\ncut some cabels."
+        scissorsButton.place(x=420,y=160,width=140,height=126)
+        scissorsButton["command"] = self.scissorsButton_command
 
         lastGameLabel=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         lastGameLabel["font"] = ft
         lastGameLabel["fg"] = "#333333"
         lastGameLabel["justify"] = "center"
-        lastGameLabel["text"] = "label"
+        lastGameLabel["text"] = "Print the result"
         lastGameLabel.place(x=40,y=300,width=519,height=52)
 
         lastGameMessage=tk.Label(root)
@@ -79,30 +79,31 @@ class App:
         lastGameMessage["font"] = ft
         lastGameMessage["fg"] = "#333333"
         lastGameMessage["justify"] = "center"
-        lastGameMessage["text"] = "label"
-        lastGameMessage.place(x=140,y=360,width=320,height=30)
+        lastGameMessage["text"] = "Here the player will get a message as result from the last round."
+        lastGameMessage.place(x=100,y=360,width=400,height=30)
 
         currentResult=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         currentResult["font"] = ft
+        currentResult["bg"] = 'white'
         currentResult["fg"] = "#333333"
         currentResult["justify"] = "center"
-        currentResult["text"] = "label"
-        currentResult.place(x=20,y=390,width=233,height=83)
+        currentResult["text"] = "Current result"
+        currentResult.place(x=10,y=390,width=150,height=83)
 
     def exitButton_command(self):
         return sys.exit()
 
 
-    def GButton_30_command(self):
+    def rockButton_command(self):
         print("command")
 
 
-    def GButton_235_command(self):
+    def paperButton_command(self):
         print("command")
 
 
-    def GButton_519_command(self):
+    def scissorsButton_command(self):
         print("command")
 
 if __name__ == "__main__":
