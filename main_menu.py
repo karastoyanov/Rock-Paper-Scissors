104417e2c2a05d559376a06389b694ab65b4c255
karastoyanov on Windows 11 DIW <karastoqnov.alexandar@gmail.com>
2023-05-04 11:02:14 +0300

    Revert "Main Menu reworked"
    This reverts commit 854a4c22f5196f488ef1aa6443492f0235918530.

--- main_menu.py
+++ main_menu.py
@@ -9,11 +9,9 @@ from PyQt5.QtWidgets import (QApplication,
                              QPlainTextEdit, 
                              QHBoxLayout, 
                              QVBoxLayout,
-                             QGroupBox,
-                             QGraphicsDropShadowEffect) 
+                             QGroupBox) 
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-# from colorama import Fore
 import sys, requests
 import db_connect, login, game
 
@@ -51,26 +49,22 @@ class MainMenu(QWidget):
         play_button.setText("play new game")
         play_button.setFont(QFont(families[0], 12))
         play_button.setFixedWidth(300)
-        play_button.setStyleSheet("background-color: #B30000; color: #000000;")
 
         all_players_ranklist = QPushButton(self)
         all_players_ranklist.setText("players ranklist")
         all_players_ranklist.setFont(QFont(families[0], 12))
         all_players_ranklist.setFixedWidth(300)
-        all_players_ranklist.setStyleSheet("background-color: #B30000; color: #000000;")
 
         edit_user_button = QPushButton(self)
         edit_user_button.setText("edit user")
         edit_user_button.setFont(QFont(families[0], 12))
         edit_user_button.setFixedWidth(300)
-        edit_user_button.setStyleSheet("background-color: #B30000; color: #000000;")
 
         logout = QPushButton(self)
         logout.clicked.connect(lambda : open_logout())
         logout.setText("logout")
         logout.setFont(QFont(families[0], 12))
         logout.setFixedWidth(300)
-        logout.setStyleSheet("background-color: #B30000; color: #000000;")
         
         right_dummy_group = QGroupBox(self)
         right_dummy_group.setFixedWidth(300)
@@ -115,68 +109,46 @@ class MainMenu(QWidget):
         user_stats_layout = QVBoxLayout()
         user_stats_layout.addStretch()
         user_stats_layout.addSpacing(5)
-
+        # Left Group
+        left_dummy_group = QGroupBox(self)
+        left_dummy_group.setFixedWidth(400)
+        left_dummy_group.setFixedHeight(300)
         # Vertical layout for the elements in the left objects group (User Name, User ID, User Rank and User Avatar)
-        user_stats_content_layout = QVBoxLayout()
+        user_stats_line_one_layout = QVBoxLayout()
         user_name = QLabel()
         db_connect.USER_POSTGRES_CURSOR.execute("SELECT current_user;")
         user_name_result = db_connect.USER_POSTGRES_CURSOR.fetchone()
         user_name_result = user_name_result[0]
         user_name.setText(f"welcome {user_name_result}")
-        user_name.setFont(QFont(families[0], 10))
-        user_name.setStyleSheet("color: #B30000;")
-        shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(15)
-        shadow.setOffset(5)
-        user_name.setGraphicsEffect(shadow)
+        user_name.setFont(QFont(families[0], 12))
+        user_name.setAlignment(Qt.AlignLeft)
         
         user_id = QLabel()
         db_connect.USER_POSTGRES_CURSOR.execute(f"SELECT user_id FROM users WHERE user_name = '{user_name_result}'")
         user_id_result = db_connect.USER_POSTGRES_CURSOR.fetchone()
         user_id_result = user_id_result[0]
         user_id.setText(f"user id: {user_id_result}")
-        user_id.setFont(QFont(families[0], 10))
-        user_id.setStyleSheet("color: #B30000;")
-        shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(15)
-        shadow.setOffset(5)
-        user_id.setGraphicsEffect(shadow)
+        user_id.setFont(QFont(families[0], 12))
+        user_id.setAlignment(Qt.AlignLeft)
         
         user_rank = QLabel()
         db_connect.USER_POSTGRES_CURSOR.execute(f"SELECT user_rank FROM users WHERE user_name = '{user_name_result}'")
         user_rank_result = db_connect.USER_POSTGRES_CURSOR.fetchone()
         user_rank_result = user_rank_result[0]
         user_rank.setText(f"rank: {user_rank_result}")
-        user_rank.setFont(QFont(families[0], 10))
-        user_rank.setStyleSheet("color: #B30000;")
-        shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(15)
-        shadow.setOffset(5)
-        user_rank.setGraphicsEffect(shadow)      
+        user_rank.setFont(QFont(families[0], 12))
+        user_rank.setAlignment(Qt.AlignLeft)        
+        # Add items to the first line on the left side
+        user_stats_line_one_layout.addWidget(user_name)
+        user_stats_line_one_layout.addWidget(user_id)
+        user_stats_line_one_layout.addWidget(user_rank)
+        # Add items to the left side
+        user_stats_layout.addLayout(user_stats_line_one_layout)
+ 
 
-        total_games = QLabel()
-        db_connect.USER_POSTGRES_CURSOR.execute(f"SELECT total_games FROM users WHERE user_name = '{user_name_result}'")
-        total_games_result = db_connect.USER_POSTGRES_CURSOR.fetchone()
-        total_games_result = total_games_result[0]
-        total_games.setText(f"total games played: {total_games_result}")
-        total_games.setFont(QFont(families[0], 10))
-        total_games.setStyleSheet("color: #B30000;")
-        shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(15)
-        shadow.setOffset(5)
-        total_games.setGraphicsEffect(shadow)
-
-        total_points = QLabel()
-        db_connect.USER_POSTGRES_CURSOR.execute(f"SELECT total_points FROM users WHERE user_name = '{user_name_result}'")
-        total_points_result = db_connect.USER_POSTGRES_CURSOR.fetchone()
-        total_points_result = total_points_result[0]
-        total_points.setText(f"total points: {total_points_result}")
-        total_points.setFont(QFont(families[0], 10))
-        total_points.setStyleSheet("color: #B30000;")
-        shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(15)
-        shadow.setOffset(5)
-        total_points.setGraphicsEffect(shadow)
+        user_stats = QVBoxLayout()
+        user_stats.addWidget(left_dummy_group)
+        user_stats.setAlignment(Qt.AlignLeft)
         
         
         
